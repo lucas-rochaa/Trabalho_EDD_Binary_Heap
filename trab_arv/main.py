08422d935589bba6351366e2fbe08d3854030bec
lucas-rochaa <67709945+lucas-rochaa@users.noreply.github.com>
2021-05-20 14:15:14 -0300

    Update main.py

--- trab_arv/main.py
+++ trab_arv/main.py
@@ -21,49 +21,50 @@ while True:
     print("(5) - Sair do programa")
     choic = int(input("Escolha: "))
 
-    #try:1
-    if choic == 1:
-        os.system("cls")
-        insert("pacientes_novos")
-        dicionario.clear()
-        value = 999
-        for a in x:
-            dicionario[value] = a[0]
-            h.put(value)
-            value= value - 1
+    try:
+        if choic == 1:
+            os.system("cls")
+            insert("pacientes_novos")
+            dicionario.clear()
+            value = 999
+            for a in x:
+                dicionario[value] = a[0]
+                h.put(value)
+                value= value - 1
 
-        excluir_linha()
-        time.sleep(5)
+            excluir_linha()
+            time.sleep(5)
 
-    elif choic == 2:
-        os.system("cls")
-        pac = h.get()
-        id_p = dicionario[pac]
-        print("Chamando o número:", str(pac),"\nNome do paciente: ", select_name(id_p), "\nCodigo do paciente: ", id_p)
-        insert_ja_chamados(id_p, pac)
-        delet(id_p)
-        time.sleep(5)
-
-
-    elif choic == 3:
-        os.system("cls")    
-        paciente = h.peek()
-        print("Proximo da fila:", str(paciente),"\nNome do paciente: ", select_name(paciente), "\nCodigo do paciente: ", dicionario[paciente])
-        time.sleep(5)
+        elif choic == 2:
+            os.system("cls")
+            pac = h.get()
+            id_p = dicionario[pac]
+            print("Chamando o número:", str(pac),"\nNome do paciente: ", select_name(id_p), "\nCodigo do paciente: ", id_p)
+            insert_ja_chamados(id_p, pac)
+            delet(id_p)
+            time.sleep(5)
 
 
-    elif choic == 4:
-        os.system("cls")
-        print(ultimo_ci())
-        time.sleep(5)
+        elif choic == 3:
+            os.system("cls")    
+            paciente = h.peek()
+            print("Proximo da fila:", str(paciente),"\nNome do paciente: ", select_name(paciente), "\nCodigo do paciente: ", dicionario[paciente])
+            time.sleep(5)
 
-    elif choic ==5:
-        os.system("cls")
-        excluir_linha()
-        print("adeus!")
-        break
+
+        elif choic == 4:
+            os.system("cls")
+            print(ultimo_ci())
+            time.sleep(5)
+
+        elif choic ==5:
+            os.system("cls")
+            excluir_linha()
+            print("adeus!")
+            break
 
     
-    #except:
+    except:
+        print("Erro")
         
 excluir_linha()
